0a2855fec6afc169b9f6f1cf5d41542a6b3bdfe4
SMV-Eletronica <mvvasques@msn.com>
2025-06-30 23:50:00 -0300

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,7 +1,7 @@
 // This is the "Offline page" service worker
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
+
 
 const CACHE = "pwabuilder-page";
 
