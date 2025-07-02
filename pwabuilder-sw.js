2bb8e18518d5e0c06383fe9b07b634730c3a9be3
SMV-Eletronica <mvvasques@msn.com>
2025-07-02 20:51:12 -0300

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -14,6 +14,7 @@ self.addEventListener("message", (event) => {
 self.addEventListener('install', async (event) => {
   event.waitUntil(
     caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage))
+    );
 });
 
 // Ativa navigationPreload para performance
