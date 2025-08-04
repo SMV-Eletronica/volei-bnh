6ada848a3a6345306c43a8fc7f6aa55bf47e3c0a
SMV-Eletronica <mvvasques@msn.com>
2025-08-04 12:38:32 -0300

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
 
-const CACHE = 'volei-bnh-offline-v4';
+const CACHE = 'volei-bnh-offline-v5';
 const offlineFallbackPage = 'offline.html';
 
 // Cache de imagens e vídeos
